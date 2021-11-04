9c3541156a41e39707ca77e5eebff082902b9efd
LuckDuracell <lukedru@yahoo.com>
2021-11-04 11:04:01 -0700

    Initial Commit

---
+++ Prime Calculator/Prime_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prime_CalculatorApp.swift
+//  Prime Calculator
+//
+//  Created by Luke Drushell on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Prime_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
